c72788df991b6fbf336d47d72bd1f78e0547b2ed
KateKwon <katekwon.git@gmail.com>
2021-10-29 22:59:21 +0900

    2
    2

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -9,6 +9,7 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 		System.out.println("TEST.....................................1");
+		System.out.println("TEST.....................................2");
 	}
 
 }
